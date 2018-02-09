1518189fff6e8bf9cdf095492970807c4024d0b0
Courd Headman <courdheadman@gmail.com>
2018-02-09 10:40:40 -0700

    Capitalization

--- homepress.php
+++ homepress.php
@@ -6,6 +6,6 @@ function HomePress() {
 <h1>Click the beautiful man to donate or he will eat you!!!</h1>
 <a href="https://www.paypal.me/courd"><img src="https://i.imgflip.com/1keb2v.gif" height="320"></a>
 </center>
-<p>to get automatic updates install and activate this <a href="https://github.com/afragen/github-updater/archive/develop.zip">GitHub updater plugin.</a></p>
+<p>To get automatic updates install and activate this <a href="https://github.com/afragen/github-updater/archive/develop.zip">GitHub updater plugin.</a></p>
   </div>';
 }
